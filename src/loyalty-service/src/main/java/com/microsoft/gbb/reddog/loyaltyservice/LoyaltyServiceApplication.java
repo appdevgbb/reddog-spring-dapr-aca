20d09d761d4aafbb819d3823958afbe45310b5a2
Kosta Petan <kosta.petan@microsoft.com>
2023-02-06 18:29:52 +0100

    init WIP

---
+++ src/loyalty-service/src/main/java/com/microsoft/gbb/reddog/loyaltyservice/LoyaltyServiceApplication.java
@@ -0,0 +1,15 @@
+package com.microsoft.gbb.reddog.loyaltyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class LoyaltyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyServiceApplication.class, args);
+    }
+
+}
