5012798da43cb03fb0f9d4033a06e6ab85f31903
Kosta Petan <kosta.petan@microsoft.com>
2023-02-08 19:56:51 +0000

    Adapt acounting service to dapr

--- src/accounting-service/src/main/java/com/microsoft/gbb/reddog/accountingservice/AccountingServiceApplication.java
+++ src/accounting-service/src/main/java/com/microsoft/gbb/reddog/accountingservice/AccountingServiceApplication.java
@@ -4,9 +4,9 @@ package com.microsoft.gbb.reddog.accountingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.kafka.annotation.EnableKafka;
+// import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
+// @EnableKafka
 @SpringBootApplication
 @EnableEurekaClient
 // @EnableCosmosRepositories
--- src/accounting-service/src/main/java/com/microsoft/gbb/reddog/accountingservice/controller/AccountingController.java
+++ src/accounting-service/src/main/java/com/microsoft/gbb/reddog/accountingservice/controller/AccountingController.java
@@ -4,6 +4,9 @@ import com.microsoft.gbb.reddog.accountingservice.dto.ChartKeyValue;
 import com.microsoft.gbb.reddog.accountingservice.dto.OrderSummaryDto;
 import com.microsoft.gbb.reddog.accountingservice.dto.OrdersTimeSeries;
 import com.microsoft.gbb.reddog.accountingservice.service.AccountingService;
+
+import io.dapr.Topic;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,25 +16,32 @@ import java.util.List;
 public class AccountingController {
 
     private final AccountingService accountingService;
+    private final String pubSubName = "reddog.pubsub";
+    private final String orderTopic = "orders";
+    private final String orderCompletedTopic = "ordercompleted";
 
     public AccountingController(AccountingService accountingService) {
         this.accountingService = accountingService;
     }
 
-    @GetMapping(value = "/orders")
-    @CrossOrigin(origins = "*")
+    /// Pub sub on Order Topic, needs to save the order to state store
+    @Topic(name = orderTopic, pubsubName = pubSubName)
+    @PostMapping(value = "/orders")
+   // @CrossOrigin(origins = "*")
     public ResponseEntity<List<OrderSummaryDto>> getAllInFlightOrders() {
         return ResponseEntity.ok(accountingService.findAllInflightOrders());
     }
 
-    @GetMapping(value = "/orders/completed")
-    @CrossOrigin(origins = "*")
+    /// Pub sub on Order Completed Topic, needs to mark the order as complete and save the order to state store
+    @Topic(name = orderCompletedTopic, pubsubName = pubSubName)
+    @PostMapping(value = "/orders/completed")
+    //@CrossOrigin(origins = "*")
     public ResponseEntity<List<OrderSummaryDto>> getAllCompletedOrders() {
         return ResponseEntity.ok(accountingService.findAllCompletedOrders());
     }
 
     @GetMapping(value = "/orders/{period}/{timeSpan}")
-    @CrossOrigin(origins = "*")
+   // @CrossOrigin(origins = "*")
     public ResponseEntity<OrdersTimeSeries> getOrdersByPeriod(@PathVariable String period,
                                                                     @PathVariable String timeSpan,
                                                                     @RequestHeader("store-id") String storeId) {
@@ -39,7 +49,7 @@ public class AccountingController {
     }
 
     @GetMapping(value = "/orders/{period}/{timeStart}/{timeEnd}")
-    @CrossOrigin(origins = "*")
+   // @CrossOrigin(origins = "*")
     public ResponseEntity<OrdersTimeSeries> getOrderCountWithinTimeInterval(@PathVariable String period,
                                                               @PathVariable String timeStart,
                                                                 @PathVariable String timeEnd,
@@ -48,7 +58,7 @@ public class AccountingController {
     }
 
     @GetMapping(value = "/orders/day")
-    @CrossOrigin(origins = "*")
+   // @CrossOrigin(origins = "*")
     public ResponseEntity<List<ChartKeyValue<Long>>> getOrdersGroupedByDay(
             @RequestHeader(value = "store-id", required = false) String storeId) {
         return ResponseEntity.ok(accountingService.getOrderCountByDay(storeId));
